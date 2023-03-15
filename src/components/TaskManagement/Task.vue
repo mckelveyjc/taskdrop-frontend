e7dcdf59e9f327a4bfb7a27e35a158042141078b
john <jmckelve@stedwards.edu>
2023-03-15 14:10:47 -0500

    cleaned up Task.vue.

--- src/components/TaskManagement/Task.vue
+++ src/components/TaskManagement/Task.vue
@@ -1,12 +1,15 @@
 <template>
     <div
-      class="task-container-template"
       id="task-container"
+      class="task-container-template"
       draggable="true"
       @dragstart="startDrag($event)">
-      <span class="standard-text"
+      <span 
+        class="standard-text"
         contenteditable
         @input="updateContent($event, this.taskInfoArray[0])">
+          <!-- displays bionic text only if global bionic state variable is set to True -->
+          <!-- taskInfoArray[2] == the name of the task -->
           <span v-if="this.bionicReaderStatus" v-html="bionicReading(this.taskInfoArray[2])"></span>
           <span v-else v-html="this.taskInfoArray[2]"></span>
       </span>
@@ -14,11 +17,11 @@
   </template>
   
   <script>
-  import { bionicReading } from 'bionic-reading';
-  import debounce from 'lodash.debounce'
-  import axios from 'axios'
   import store from '../../store'
   import { computed } from '@vue/runtime-core'
+  import { bionicReading } from 'bionic-reading'
+  import debounce from 'lodash.debounce'
+  import axios from 'axios'
 
   export default {
     name: 'Task',
@@ -41,26 +44,25 @@
       }
     },
     methods: {
-        // testing
+        // this method takes the object of Tasks (passed in by parent) & extracts the data by
+        //   creating an array, which is used to display the task names
         logTasks() {
             if (this.taskInfoObject != undefined){
                  this.taskInfoArray = Object.values(this.taskInfoObject)
             }
         },
+        // when a task is dragged, the data from the task must be recorded so that it can be updated in the database
         startDrag(evt) {
-            console.log("drag started");
             evt.dataTransfer.setData('text/plain', evt.target.id);
             evt.dataTransfer.setData('task-name', this.taskInfoArray[2]);
-
-            console.log("task name: " + this.taskInfoArray[2]);
-
             evt.dataTransfer.setData('task-id', this.taskInfoArray[0]);
             evt.dataTransfer.setData('task-info-object', this.taskInfoObject);
             setTimeout(() => { 
                 evt.target.style.display = "none";
             }, 0);
         },
-        // Debounce used here to make sure we don't send too many requests to the server
+        // this method updates the task name if it is changed
+        // debounce used here to make sure we don't send too many requests to the server
         updateContent: debounce((evt, taskID) => {
           const path = 'http://157.230.93.52/update-task/update-name'
           axios.post(path, {
@@ -80,7 +82,6 @@
   </script>
   
   <style scoped>
-    /* globalize */
     @import "../../assets/global.css";
     #task-container {
       background-color: #F06AF3;
